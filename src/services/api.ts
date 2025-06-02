521a4f9b6636c88e75540f660abe09812ef44e8b
Beri28 <cletusberinyuy@gmail.com>
2025-06-02 04:21:35 +0100

    update

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,6 +1,7 @@
 import { StudentResult, User } from '../types/types';
 
-const API_BASE_URL = 'http://localhost:5000/api/v1'; // This will be proxied to your backend
+// const API_BASE_URL = 'http://localhost:5000/api/v1'; // This will be proxied to your backend
+const API_BASE_URL = 'https://student-portal-backend-vcfj.onrender.com/api/v1'; // This will be proxied to your backend
 
 export const fetchResults = async (studentId?: string, semester?: string): Promise<any> => {
   const response = await fetch(`${API_BASE_URL}/results?studentId=${studentId}&semester=${semester}`);
